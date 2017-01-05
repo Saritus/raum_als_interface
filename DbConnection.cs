b826a3128e7cdca0d98c85c93c7862a12f353b60
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-05 19:23:57 +0100

    Implement connect method

--- DbConnection.cs
+++ DbConnection.cs
@@ -18,7 +18,18 @@ class DbConnection
     public bool connect()
     {
         // TODO: connect to mariaDB / MySQL database
-        return false;
+        try
+        {
+            string connsqlstring = "Server=your.ip.address;Port=3306;database=YOUR_DATA_BASE;User Id=root;Password=password;charset=utf8";
+            sqlconn = new MySqlConnection(connsqlstring);
+            sqlconn.Open();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
     }
 
     public bool close()
