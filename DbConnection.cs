43fcf0dab9d0f3f214538eb14a840b3cbcfa22f6
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-05 18:00:31 +0100

    Add example connection to constructor
    This is anything but final. Just an example from the official xamarin
    website for mysql connections

--- DbConnection.cs
+++ DbConnection.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace TouchWalkthrough
 {
@@ -6,7 +9,22 @@ namespace TouchWalkthrough
     {
         public DbConnection()
         {
-
+            try
+            {
+                new I18N.West.CP1250();
+                MySqlConnection sqlconn;
+                string connsqlstring = "Server=your.ip.address;Port=3306;database=YOUR_DATA_BASE;User Id=root;Password=password;charset=utf8";
+                sqlconn = new MySqlConnection(connsqlstring);
+                sqlconn.Open();
+                string queryString = "select count(0) from ACCOUNT";
+                MySqlCommand sqlcmd = new MySqlCommand(queryString, sqlconn);
+                string result = sqlcmd.ExecuteScalar().ToString();
+                sqlconn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public bool connect()
