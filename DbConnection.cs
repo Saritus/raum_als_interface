b3f5a624d6669e1d27beef81262913fae472fe2a
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-05 19:02:16 +0100

    Remove example functions

--- DbConnection.cs
+++ DbConnection.cs
@@ -9,49 +9,7 @@ class DbConnection
 {
     public DbConnection()
     {
-        try
-        {
-            new I18N.West.CP1250();
-            MySqlConnection sqlconn;
-            string connsqlstring = "Server=your.ip.address;Port=3306;database=YOUR_DATA_BASE;User Id=root;Password=password;charset=utf8";
-            sqlconn = new MySqlConnection(connsqlstring);
-            sqlconn.Open();
-            string queryString = "select count(0) from ACCOUNT";
-            MySqlCommand sqlcmd = new MySqlCommand(queryString, sqlconn);
-            string result = sqlcmd.ExecuteScalar().ToString();
-            sqlconn.Close();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-    }
-
-    public List<String> LoadAllItemFromMySQL()
-    {
-        List<String> products = new List<String>();
-        try
-        {
-            string connsqlstring = "Server=your.ip.address;Port=3306;database=YOUR_DATA_BASE;User Id=root;Password=password;charset=utf8";
-            MySqlConnection sqlconn = new MySqlConnection(connsqlstring);
-            sqlconn.Open();
-
-            DataSet tickets = new DataSet();
-            string queryString = "select item.NAME from ITEM as item";
-            MySqlDataAdapter adapter = new MySqlDataAdapter(queryString, sqlconn);
-            adapter.Fill(tickets, "Item");
-            foreach (DataRow row in tickets.Tables["Item"].Rows)
-            {
-                products.Add(row[0].ToString());
-            }
-
-            sqlconn.Close();
-        }
-        catch (Exception e)
-        {
-            Console.Write(e.Message);
-        }
-        return products;
+        // TODO: create a new db connection
     }
 
     public bool connect()
