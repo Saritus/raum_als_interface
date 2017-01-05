719b18d1ba1514ca8853caca7151c777969c52ac
Tom I <tom.ille@gmx.de>
2017-01-05 18:57:57 +0100

    Add interfaces and base classes
    Interface for ServiceHandler and Frontend-Backend communication on
    client side (UIHandler);
    Classes for Drop, Event:Drop, Voting:Drop, Warning:Drop,
    ExtendedLocation (GPSLoc., building, floor);
    Enums for Category (may not be needed), Building

---
+++ Category.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouchWalkthrough
+{
+    enum Category { EVENT, WARNING, VOTE };
+}
---
+++ Drop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TouchWalkthrough
+{
+
+    class Drop
+    {
+        private int id              { get; set; }
+        private string name         { get; set; }
+        private string description  { get; set; }
+        private Category category   { get; set; }
+
+        private DateTime startTime  { get; set; }
+        private DateTime endTime    { get; set; }
+
+        private ExtendedLocation location { get; set; }
+
+        private bool followed       { get; set; }
+        private bool ignored        { get; set; }
+
+        private string picturePath { get; set; }
+    }
+}
