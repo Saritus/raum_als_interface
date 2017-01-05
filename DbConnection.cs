510e645bd2bc964c9dd588339ffe8e21adb1af91
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-05 17:51:44 +0100

    Add prototype methodes to dbconnection

--- DbConnection.cs
+++ DbConnection.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TouchWalkthrough
 {
     class DbConnection
@@ -5,6 +7,24 @@ namespace TouchWalkthrough
         public DbConnection()
         {
 
+        }
+
+        public bool connect()
+        {
+            // TODO: connect to mariaDB / MySQL database
+            return true;
+        }
+
+        Drop[] getNewEvents(DateTime lastTime)
+        {
+            // TODO: return fetched drops from database
+            return null;
+        }
+
+        bool saveNewEvent(Drop newDrop)
+        {
+            // TODO: push new drop to database
+            return true;
         }
     }
 }
