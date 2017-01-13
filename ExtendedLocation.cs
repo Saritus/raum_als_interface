f201ef10e09a0f446bbd8b75bbe74da244678d3a
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-13 01:22:48 +0100

    Add constructors to ExtendedLocation

--- ExtendedLocation.cs
+++ ExtendedLocation.cs
@@ -1,9 +1,22 @@
 using Android.Locations;
+using System;
 
 namespace TouchWalkthrough
 {
-    public class ExtendedLocation : Location{
-	    private Building building 	{ get; set; }
-	    private int floor 			{ get; set; }
+    public class ExtendedLocation : Location
+    {
+        private Building building { get; set; }
+        private int floor { get; set; }
+
+
+        public ExtendedLocation(String provider) : base(provider)
+        {
+
+        }
+
+        public ExtendedLocation(Location l) : base(l)
+        {
+
+        }
     }
 }
