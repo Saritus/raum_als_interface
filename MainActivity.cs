fbc31bb4ca00a551cee82676fbe1ed6aa1f57800
Julian93MI <Julian@JulianMac.local>
2017-01-17 18:53:50 +0100

    Update GUI + MainActivity Logik...
    neue Icons geladen
    neue Layouts erstellt
    MainActivity: Button OnClick-Events und Listview erstellt

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,89 +1,94 @@
 ﻿namespace TouchWalkthrough
 {
+	using System;
     using Android.App;
     using Android.OS;
     using Android.Views;
     using Android.Widget;
+	using System.Collections.Generic; //For ListView
 
-
-    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/ic_launcher")]
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/Icon")]
 	public class MainActivity : Activity
 	{
-        bool plus_button_on = false;
-        bool filter_button_on = false;
+		bool history_button_on = false;
+		bool filter_button_on = false;
+
+		//For ListView
+		private ListView listnames;
+		private List<string> itemlist;
+
 
         protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-            //SetContentView(Resource.Layout.Main); 
-            //View v = new GestureRecognizerView(this);
-            //SetContentView(v);
+			SetContentView(Resource.Layout.Main);
 
 
-            //Versuch 1:
-            View v = new GestureRecognizerView(this);
-            LinearLayout karte = new LinearLayout(this);
-            LinearLayout.LayoutParams linearLayoutParams2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WrapContent,
-            LinearLayout.LayoutParams.WrapContent, 3.0F);
-            LinearLayout.LayoutParams linearLayoutParams1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WrapContent,
-            LinearLayout.LayoutParams.WrapContent, 1.0F);
-            karte.LayoutParameters = linearLayoutParams1;
-            v.LayoutParameters = linearLayoutParams2;
-            karte.AddView(v);
-            SetContentView(karte);
+			//For ListView
+			listnames = FindViewById<ListView>(Resource.Id.historyList);
+
+			itemlist = new List<string>();
+			itemlist.Add("Item 0");
+			itemlist.Add("Item 1");
+			itemlist.Add("Item 2");
+			itemlist.Add("Item 3");
+			itemlist.Add("Item 4");
+
+			ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1,itemlist);
+			listnames.Adapter = adapter;
+
+			listnames.ItemClick += Listnames_ItemClick;
+			//For ListView ENDE
 
 
+			//For History-Button
+			ImageButton history_button = FindViewById<ImageButton>(Resource.Id.imageButton6);
+			LinearLayout showHistoryLayout = FindViewById<LinearLayout>(Resource.Id.linearLayoutHistory);
+
+			history_button.Click += (object sender, EventArgs e) =>
+			{
+				if (history_button_on == false)
+				{
+					history_button.SetImageResource(Resource.Drawable.plus_button);
+					showHistoryLayout.Visibility = ViewStates.Visible;
+				}
+				else {
+					history_button.SetImageResource(Resource.Drawable.history_button);
+					showHistoryLayout.Visibility = ViewStates.Gone;
+				}
+			};
+			//For History-Button ENDE
 
 
+			//For Filter-Button
+			ImageButton filter_button = FindViewById<ImageButton>(Resource.Id.imageButton2);
+			LinearLayout showFilterLayout = FindViewById<LinearLayout>(Resource.Id.linearLayoutFilter);
 
-
-            /*VERSUCH 2:
-            View v = new GestureRecognizerView(this);
-            //LinearLayout layout = FindViewById<LinearLayout>(Resource.Layout.Main);
-            LinearLayout l = new LinearLayout(this);
-            l.AddView(v);
-            LinearLayout.LayoutParams linearLayoutParams2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WrapContent,
-            LinearLayout.LayoutParams.WrapContent,3.0F);
-            l.LayoutParameters = linearLayoutParams2;
-            LinearLayout mainlayout = new LinearLayout(this);
-            LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FillParent,
-            LinearLayout.LayoutParams.FillParent,1.0F);
-            mainlayout.LayoutParameters = linearLayoutParams;
-
-            mainlayout.AddView(l);
-            //l.AddView(layout);
-            SetContentView(mainlayout);*/
-
-
-
-            /* VERSUCH 3:
-            LinearLayout linearLayout = new LinearLayout(this);
-            LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FillParent,
-            LinearLayout.LayoutParams.FillParent);
-            linearLayoutParams.SetMargins(0, 0, 0, 0);
-            linearLayout.LayoutParameters = linearLayoutParams;
-
-            Button plus_test = FindViewById<Button>(Resource.Id.Button_plus);
-            plus_test.SetBackgroundResource(Resource.Drawable.plus_button_normal);
-
-            linearLayout.AddView(plus_test);
-            SetContentView(linearLayout);*/
-
-
-
-
-
-
-
-
-
-
-
-
+			filter_button.Click += (object sender, EventArgs e) =>
+			{
+				if (filter_button_on == false)//Öffnet Untermenü
+				{
+					showFilterLayout.Visibility = ViewStates.Visible;
+				}
+				else {//Schließt Untermenü
+					showFilterLayout.Visibility = ViewStates.Gone;
+				}
+			};
+			//For Filter-Button ENDE
 
 
 
         }
+
+
+		//For ListView
+		private void Listnames_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+		{
+			Toast.MakeText(this, e.Position.ToString(), ToastLength.Long).Show();
+		}
+		//For ListView ENDE
+
+
     }
 }
 
