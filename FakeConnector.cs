86cd15d49c0461a328a4465a67c8e9ab4b809138
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-19 22:50:53 +0100

    Create dummyDrops on first get

--- FakeConnector.cs
+++ FakeConnector.cs
@@ -6,7 +6,27 @@ namespace TouchWalkthrough
 {
     abstract class FakeConnector
     {
-        static List<Drop> dummyDrops;
+        static List<Drop> dummyDrops
+        {
+            get
+            {
+                if (!dummyDropsCreated)
+                {
+                    // Create dummy drops
+                    foreach (Category filter in Enum.GetValues(typeof(Category)))
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            string f = Enum.GetName(typeof(Category), filter);
+                            dummyDrops.Add(new Drop(i, "Test" + f + i, filter, "Test" + f + i + "Description, ", DateTime.Today, DateTime.Now, new ExtendedLocation()));
+                        }
+                    }
+
+                }
+                return dummyDrops;
+            }
+        }
+        static bool dummyDropsCreated = false;
 
         private static bool connect()
         {
