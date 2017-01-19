87cf487ed50f3c76106b511d584befccbdea2547
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-19 17:27:45 +0100

    Remove useless getter and setter functions

--- Drop.cs
+++ Drop.cs
@@ -41,66 +41,6 @@ namespace TouchWalkthrough
             this.picturePath = picPath;
         }
 
-        public int getId()
-        {
-            return id;
-        }
-
-        public string getName()
-        {
-            return name;
-        }
-
-        public string getDescription()
-        {
-            return description;
-        }
-
-        public Category getCategory()
-        {
-            return category;
-        }
-
-        public DateTime getStartTime()
-        {
-            return startTime;
-        }
-
-        public DateTime getEndTime()
-        {
-            return endTime;
-        }
-
-        public ExtendedLocation getLocation()
-        {
-            return null;
-        }
-
-        public bool isFollowed()
-        {
-            return followed;
-        }
-
-        public void follow(bool i)
-        {
-            this.followed = i;
-        }
-
-        public bool isIgnored()
-        {
-            return ignored;
-        }
-
-        public void ignore(bool i)
-        {
-            this.ignored = i;
-        }
-
-        public string getPicturePath()
-        {
-            return picturePath;
-        }
-
         public Drop(string xmlfile)
         {
             // TODO: load a drop from a xml file
