77477d5f406bf1cd8f9b65d4e0c996c627d2c1a7
Julian93MI <Julian@JulianMac.local>
2017-01-19 12:43:29 +0100

    Hardware-Button+No-Titel-Bar
    - Hardware-zurück-Button implementiert (funktioniert nicht korrekt)
    - Eingestellt, dass Titelbar nicht sichtbar
    
    - GUI für „neuen Drop erstellen“ begonnen

--- MainActivity.cs
+++ MainActivity.cs
@@ -7,11 +7,13 @@
     using Android.Widget;
     using System.Collections.Generic; //For ListView
 
-    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/Icon")]
+	//Theme = "@android:style/Theme.NoTitleBar"
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/Icon", Theme = "@android:style/Theme.NoTitleBar")]
     public class MainActivity : Activity
     {
         bool history_button_on = false;
         bool filter_button_on = false;
+		bool plus_button_on = false;
 
         //For ListView
         private ListView listnames;
@@ -23,9 +25,9 @@
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
-            /*
+            
 			//For ListView
-			listnames = FindViewById<ListView>(Resource.Id.historyList);
+			/*listnames = FindViewById<ListView>(Resource.Id.historyList);
 
 			itemlist = new List<string>();
 			itemlist.Add("Item 0");
@@ -37,33 +39,29 @@
 			ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1,itemlist);
 			listnames.Adapter = adapter;
 
-			listnames.ItemClick += Listnames_ItemClick;
+			listnames.ItemClick += Listnames_ItemClick;*/
 			//For ListView ENDE
-            */
+            
 
-            //For History-Button
+            //For History-Button ##############################################################
             ImageButton history_button = FindViewById<ImageButton>(Resource.Id.imageButton6);
-            LinearLayout showHistoryLayout = FindViewById<LinearLayout>(Resource.Id.linearLayoutHistory);
-
             history_button.Click += (object sender, EventArgs e) =>
             {
                 if (history_button_on == false)
                 {
-                    history_button.SetImageResource(Resource.Drawable.plus_button);
-                    //showHistoryLayout.Visibility = ViewStates.Visible;
+                    SetContentView(Resource.Layout.History);
                     history_button_on = true;
-                }
+                } 
                 else
-                {
-                    history_button.SetImageResource(Resource.Drawable.history_button);
-                    //showHistoryLayout.Visibility = ViewStates.Gone;
+                { 
+                    SetContentView(Resource.Layout.Main);
                     history_button_on = false;
                 }
             };
-            //For History-Button ENDE
+            //For History-Button ENDE ##############################################################
 
 
-            //For Filter-Button
+            //For Filter-Button ##############################################################
             ImageButton filter_button = FindViewById<ImageButton>(Resource.Id.imageButton2);
             RelativeLayout showFilterLayout = FindViewById<RelativeLayout>(Resource.Id.relativeLayoutFilter);
 
@@ -80,19 +78,59 @@
                     filter_button_on = false;
                 }
             };
-            //For Filter-Button ENDE
+            //For Filter-Button ENDE ##############################################################
 
 
 
-        }
+			//For Plus-Button ##############################################################
+			ImageButton plus_button = FindViewById<ImageButton>(Resource.Id.imageButton1);
+			plus_button.Click += (object sender, EventArgs e) =>
+			{
+				if (plus_button_on == false)//Öffnet Untermenü
+				{
+					SetContentView(Resource.Layout.Plus_Menue);
+					plus_button_on = true;
+				}
+				else
+				{//Schließt Untermenü
+					SetContentView(Resource.Layout.Main);
+					plus_button_on = false;
+				}
+			};
+			//For Plus-Button ENDE ##############################################################
 
 
-        //For ListView
+
+			 
+
+	}
+
+		//Use Hardware-Back-Button ##############################################################
+		public override void OnBackPressed()
+		{
+			//base.OnBackPressed();
+			if (plus_button_on == true)
+			{ //gehe zurück auf Main-Screen
+				plus_button_on = false;
+				SetContentView(Resource.Layout.Main);
+			}
+
+			if (history_button_on == true)
+			{//gehe zurück auf Main-Screen
+				history_button_on = false;
+				//SetContentView(Resource.Layout.Main);
+			}
+
+		}
+		//Use Hardware-Back-Button ENDE ##############################################################
+
+
+        //For ListView ##############################################################
         private void Listnames_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
         {
             Toast.MakeText(this, e.Position.ToString(), ToastLength.Long).Show();
         }
-        //For ListView ENDE
+        //For ListView ENDE ##############################################################
 
 
     }
