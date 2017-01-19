369b89925bfd81b6f745bbb35445fdcc8567c8b4
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-19 22:28:28 +0100

    Add FakeConnector

---
+++ FakeConnector.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace TouchWalkthrough
+{
+    abstract class FakeConnector
+    {
+
+        private static bool connect()
+        {
+            return true;
+        }
+
+        private static void close()
+        {
+
+        }
+
+        public static List<Drop> getNewDrops(DateTime lastUpdate)
+        {
+            return null;
+        }
+
+        public static bool saveNewDrop(Drop newDrop)
+        {
+            return false;
+        }
+    }
+}
