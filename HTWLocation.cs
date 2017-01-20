8a7e6037cdd06b26aef7f763abb66b0290f7ef51
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-20 22:48:08 +0100

    Implement HTWLocation(string room)

--- HTWLocation.cs
+++ HTWLocation.cs
@@ -1,18 +1,30 @@
 using Android.Locations;
 using System;
+using System.Text.RegularExpressions;
 
 namespace TouchWalkthrough
 {
     public class HTWLocation
     {
         public Building building { get; set; }
+
         public int floor { get; set; }
 
         public int room { get; set; }
 
         public HTWLocation(string room)
         {
-
+            Regex regex = new Regex("[A-Z][0-9]+");
+            if (regex.IsMatch(room))
+            {
+                this.building = (Building)Enum.Parse(typeof(Building), room[0].ToString());
+                this.floor = int.Parse(room[1].ToString());
+                this.room = int.Parse(room.Substring(1));
+            }
+            else
+            {
+                throw new Exception("Invalid room");
+            }
         }
 
         public HTWLocation()
