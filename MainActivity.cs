9676d0ac8465a1e95eaa065da0cb2deec7c7d01b
Sebastian Mischke <SebastianMischke@gmx.de>
2017-02-04 03:43:03 +0100

    Use ToImageButton

--- MainActivity.cs
+++ MainActivity.cs
@@ -42,22 +42,7 @@
                 ImageView kartenlayout = FindViewById<ImageView>(Resource.Id.ImageViewKarte);
                 foreach (Drop mapdrop in mapDrops)
 				{
-					ImageButton drop_button = new ImageButton(this);
-
-                    // Aussehen
-					switch (mapdrop.category)
-					{
-						case Category.EVENT:
-							drop_button.SetImageResource(Resource.Drawable.icon_hap1);
-							break;
-						case Category.WARNING:
-							drop_button.SetImageResource(Resource.Drawable.icon_hap2);
-							break;
-						case Category.VOTE:
-							drop_button.SetImageResource(Resource.Drawable.icon_hap3);
-							break;
-					}
-					drop_button.SetBackgroundColor(Color.Transparent);
+					ImageButton drop_button = mapdrop.ToImageButton(this);
 
                     // Position
                     float left = ((float)kartenlayout.Width - ((float)1224 / (float)2176) * (float)kartenlayout.Height) / (float)2;
